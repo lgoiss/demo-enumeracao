da9a91efa1e09c695a5aafcf3de3b9b299dea890
Luzivan Gois <luzivangoiss@gmail.com>
2023-07-18 16:22:11 -0300

    Correção do código de enumeração de endpoints

--- enumEndPoints.py
+++ enumEndPoints.py
@@ -1,16 +1,19 @@
 import requests
 import json
 
+url = 'http://localhost:8080/'
 
 with open("wordlist/endpoints.txt", "r") as arquivo:
     linhas = arquivo.readlines()
 
     for linha in linhas:
     
-        endopoint  = linha.strip()
-        r = requests.get('http://localhost:8080/'+endopoint)
+        endpoint  = linha.strip()
+        
+        r = requests.get(url+endpoint)
         retorno = r.status_code
-        codigo_retorno   = str(retorno)
-        if (retorno != 200):
+        codigo_retorno = str(retorno)
+        if (retorno != 404):
             with open("endpoints_validos.txt", "a") as resultado:
-                resultado.write("\nCódigo: "+codigo_retorno+"\nEndpoint Encontrado: http://localhost:8080/"+endopoint)
+                resultado.write("\nCódigo: "+codigo_retorno+"\nEndpoint Encontrado: "+url+endpoint)
+
